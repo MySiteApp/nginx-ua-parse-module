ccb1db152bbc33a9d0c3486838e01073c7f652f7
Kobi Meirson <kobi@brow.si>
2014-02-20 19:30:18 +0200

    Meanwhile

--- ngx_http_ua_parse_module.c
+++ ngx_http_ua_parse_module.c
@@ -6,45 +6,54 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
- 
- 
-static char *ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd,
+
+typedef struct {
+    
+    ngx_array_t *devices;
+    ngx_array_t *browsers;
+    ngx_array_t *os;
+} ngx_http_ua_parse_conf_t;
+
+static ngx_int_t ngx_http_ua_parse_add_variables(ngx_conf_t *cf);
+
+static char *ngx_http_ua_parse_list(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
+    
+static ngx_int_t ngx_http_ua_parse_agent_variable(ngx_http_request_t *r,
+    ngx_http_variable_value_t *v, uintptr_t data);
+    
+static ngx_int_t ngx_http_ua_parse_add_variables(ngx_conf_t *cf);
  
-static ngx_command_t ngx_http_hello_commands[] = {
-    { ngx_string("hello"),
-      NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
-      ngx_http_hello,
-      0,
+static ngx_command_t ngx_http_ua_parse_commands[] = {
+    { ngx_string("uaparse_list"),
+      NGX_HTTP_MAIN_CONF|NGX_CONF_TAKE1,
+      ngx_http_ua_parse_list,
+      NGX_HTTP_MAIN_CONF_OFFSET,
       0,
       NULL },
  
     ngx_null_command
 };
  
+static ngx_http_module_t ngx_http_ua_parse_module_ctx = {
+    ngx_http_ua_parse_add_variables,    /* preconfiguration */
+    NULL,                               /* postconfiguration */
  
-static u_char ngx_hello_string[] = "Hello, world!";
+    NULL,                               /* create main configuration */
+    NULL,                               /* init main configuration */
  
+    NULL,                               /* create server configuration */
+    NULL,                               /* merge server configuration */
  
-static ngx_http_module_t ngx_http_hello_module_ctx = {
-    NULL,                          /* preconfiguration */
-    NULL,                          /* postconfiguration */
- 
-    NULL,                          /* create main configuration */
-    NULL,                          /* init main configuration */
- 
-    NULL,                          /* create server configuration */
-    NULL,                          /* merge server configuration */
- 
-    NULL,                          /* create location configuration */
-    NULL                           /* merge location configuration */
+    NULL,                               /* create location configuration */
+    NULL                                /* merge location configuration */
 };
  
  
-ngx_module_t ngx_http_hello_module = {
+ngx_module_t ngx_http_ua_parse_module = {
     NGX_MODULE_V1,
-    &ngx_http_hello_module_ctx,    /* module context */
-    ngx_http_hello_commands,       /* module directives */
+    &ngx_http_ua_parse_module_ctx, /* module context */
+    ngx_http_ua_parse_commands,    /* module directives */
     NGX_HTTP_MODULE,               /* module type */
     NULL,                          /* init master */
     NULL,                          /* init module */
@@ -55,79 +64,43 @@ ngx_module_t ngx_http_hello_module = {
     NULL,                          /* exit master */
     NGX_MODULE_V1_PADDING
 };
- 
- 
-static ngx_int_t
-ngx_http_hello_handler(ngx_http_request_t *r)
+
+static ngx_http_variable_t ngx_http_ua_parse_vars[] = {
+
+    { ngx_string("ua_parse_agent"), NULL,
+      ngx_http_ua_parse_agent_variable,
+      0, 0, 0 },
+      
+    { ngx_null_string, NULL, NULL, 0, 0, 0 }
+};
+
+static ngx_int_t ngx_http_ua_parse_agent_variable(ngx_http_request_t *r,
+    ngx_http_variable_value_t *v, uintptr_t data)
 {
-    ngx_int_t    rc;
-    ngx_buf_t   *b;
-    ngx_chain_t  out;
- 
-    /* we response to 'GET' and 'HEAD' requests only */
-    if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_HEAD))) {
-        return NGX_HTTP_NOT_ALLOWED;
-    }
- 
-    /* discard request body, since we don't need it here */
-    rc = ngx_http_discard_request_body(r);
- 
-    if (rc != NGX_OK) {
-        return rc;
-    }
- 
-    /* set the 'Content-type' header */
-    r->headers_out.content_type_len = sizeof("text/html") - 1;
-    r->headers_out.content_type.len = sizeof("text/html") - 1;
-    r->headers_out.content_type.data = (u_char *) "text/html";
- 
-    /* send the header only, if the request type is http 'HEAD' */
-    if (r->method == NGX_HTTP_HEAD) {
-        r->headers_out.status = NGX_HTTP_OK;
-        r->headers_out.content_length_n = sizeof(ngx_hello_string) - 1;
- 
-        return ngx_http_send_header(r);
-    }
- 
-    /* allocate a buffer for your response body */
-    b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
-    if (b == NULL) {
-        return NGX_HTTP_INTERNAL_SERVER_ERROR;
-    }
- 
-    /* attach this buffer to the buffer chain */
-    out.buf = b;
-    out.next = NULL;
- 
-    /* adjust the pointers of the buffer */
-    b->pos = ngx_hello_string;
-    b->last = ngx_hello_string + sizeof(ngx_hello_string) - 1;
-    b->memory = 1;    /* this buffer is in memory */
-    b->last_buf = 1;  /* this is the last buffer in the buffer chain */
- 
-    /* set the status line */
-    r->headers_out.status = NGX_HTTP_OK;
-    r->headers_out.content_length_n = sizeof(ngx_hello_string) - 1;
- 
-    /* send the headers of your response */
-    rc = ngx_http_send_header(r);
- 
-    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
-        return rc;
-    }
- 
-    /* send the buffer chain of your response */
-    return ngx_http_output_filter(r, &out);
+    v->not_found = 1;
+    return NGX_OK;
 }
- 
- 
+
 static char *
-ngx_http_hello(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+ngx_http_ua_parse_list(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
-    ngx_http_core_loc_conf_t *clcf;
- 
-    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-    clcf->handler = ngx_http_hello_handler; /* handler to process the 'hello' directive */
- 
+    ngx_log_error(NGX_LOG_ERR, cf->log, 0, "Sup?", );
     return NGX_CONF_OK;
+}
+
+static ngx_int_t
+ngx_http_ua_parse_add_variables(ngx_conf_t *cf)
+{
+    ngx_http_variable_t *var, *v;
+    
+    for (v = ngx_http_ua_parse_vars; v->name.len; v++) {
+        var = ngx_http_add_variable(cf, &v->name, v->flags);
+        if (var == NULL) {
+            return NGX_ERROR;
+        }
+        var->get_handler = v->get_handler;
+        var->data = v->data;
+    }
+    
+    return NGX_OK;
 }
