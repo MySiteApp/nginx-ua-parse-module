adac5142ef962bd06612d4101f2172cc1fadeb3a
Kobi Meirson <kobi@brow.si>
2014-03-02 20:04:20 +0200

    - Adding Eclipse CDT project exclusions - Supporting REGEXes with replacements (in the form of sprintf)

--- ngx_http_ua_parse_module.c
+++ ngx_http_ua_parse_module.c
@@ -10,7 +10,11 @@
 #include "cJSON/cJSON.h"
 
 typedef struct {
-    
+	ngx_regex_compile_t rgc;
+	ngx_str_t			*replacement;
+} ngx_http_ua_parse_elem_t;
+
+typedef struct {
     ngx_array_t *devices;
     ngx_array_t *browsers;
     ngx_array_t *os;
@@ -21,10 +25,13 @@ static ngx_int_t ngx_http_ua_parse_add_variables(ngx_conf_t *cf);
 static char *ngx_http_ua_parse_list(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
     
-static ngx_int_t ngx_http_ua_parse_agent_variable(ngx_http_request_t *r,
+static ngx_int_t ngx_http_ua_parse_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data);
     
-static ngx_int_t ngx_http_ua_parse_add_variables(ngx_conf_t *cf);
+static void * ngx_http_ua_parse_create_conf(ngx_conf_t *cf);
+static void ngx_http_ua_parse_cleanup(void *data);
+
+static ngx_array_t * ngx_http_ua_parse_load_from_json(ngx_conf_t *cf, cJSON *current);
  
 static ngx_command_t ngx_http_ua_parse_commands[] = {
     { ngx_string("uaparse_list"),
@@ -41,7 +48,7 @@ static ngx_http_module_t ngx_http_ua_parse_module_ctx = {
     ngx_http_ua_parse_add_variables,    /* preconfiguration */
     NULL,                               /* postconfiguration */
  
-    NULL,                               /* create main configuration */
+    ngx_http_ua_parse_create_conf,      /* create main configuration */
     NULL,                               /* init main configuration */
  
     NULL,                               /* create server configuration */
@@ -67,29 +74,188 @@ ngx_module_t ngx_http_ua_parse_module = {
     NGX_MODULE_V1_PADDING
 };
 
+#define NGX_UA_PARSE_DEVICE_FAMILY 0
+#define NGX_UA_PARSE_OS_FAMILY 1
+#define NGX_UA_PARSE_AGENT_FAMILY 2
+
 static ngx_http_variable_t ngx_http_ua_parse_vars[] = {
+    { ngx_string("ua_parse_device"), NULL,
+      ngx_http_ua_parse_variable,
+      NGX_UA_PARSE_DEVICE_FAMILY, 0, 0 },
+
+    { ngx_string("ua_parse_os"), NULL,
+	  ngx_http_ua_parse_variable,
+	  NGX_UA_PARSE_OS_FAMILY, 0, 0 },
 
     { ngx_string("ua_parse_agent"), NULL,
-      ngx_http_ua_parse_agent_variable,
-      0, 0, 0 },
+      ngx_http_ua_parse_variable,
+      NGX_UA_PARSE_AGENT_FAMILY, 0, 0 },
       
     { ngx_null_string, NULL, NULL, 0, 0, 0 }
 };
 
-static ngx_int_t ngx_http_ua_parse_agent_variable(ngx_http_request_t *r,
+static void *
+ngx_http_ua_parse_create_conf(ngx_conf_t *cf)
+{
+    ngx_pool_cleanup_t     *cln;
+    ngx_http_ua_parse_conf_t  *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_ua_parse_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+
+    cln = ngx_pool_cleanup_add(cf->pool, 0);
+    if (cln == NULL) {
+        return NULL;
+    }
+
+    cln->handler = ngx_http_ua_parse_cleanup;
+    cln->data = conf;
+
+    return conf;
+}
+
+// Get family
+static ngx_int_t ngx_http_ua_parse_variable(ngx_http_request_t *r,
     ngx_http_variable_value_t *v, uintptr_t data)
 {
-    v->not_found = 1;
+    ngx_http_ua_parse_conf_t    *upcf;
+    ngx_uint_t                  n, i;
+    ngx_http_ua_parse_elem_t   	*ptr, *cur;
+    int                         rc, *captures;
+    ngx_array_t					*lst;
+    ngx_str_t					str;
+    u_char						*p, *foundStr;
+    
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Cur UA is \"%s\"", r->headers_in.user_agent->value.data);
+    
+    upcf = ngx_http_get_module_main_conf(r, ngx_http_ua_parse_module);
+
+    switch (data) {
+    case NGX_UA_PARSE_DEVICE_FAMILY:
+    	lst = upcf->devices;
+    	break;
+    case NGX_UA_PARSE_OS_FAMILY:
+    	lst = upcf->os;
+    	break;
+    case NGX_UA_PARSE_AGENT_FAMILY:
+    	lst = upcf->browsers;
+    	break;
+    default:
+    	goto not_found;
+    }
+
+    if (lst == NULL) {
+        goto not_found;
+    }
+
+    ptr = lst->elts;
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Total of %d in array", upcf->os->nelts);
+    for (i = 0; i < lst->nelts; i++) {
+        cur = &ptr[i];
+        n = (cur->rgc.captures + 1) * 3;
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Cur n is %d (\"%s\")", n, cur->rgc.pattern.data);
+        captures = ngx_palloc(r->pool, n * sizeof(int));
+        rc = ngx_regex_exec(cur->rgc.regex, &r->headers_in.user_agent->value, captures, n);
+        if (rc < 0) {
+        	continue;
+        }
+        // Match the first one (captures[2] is the start, captures[3] is the end)
+        str.data = (u_char *) (r->headers_in.user_agent->value.data + captures[2]);
+        str.len = captures[3] - captures[2];
+
+        if (cur->replacement) {
+        	// Copy the string to the foundStr place...
+        	foundStr = ngx_alloc((str.len + 1) * sizeof(u_char), r->connection->log);
+        	ngx_memzero(foundStr, (str.len + 1) * sizeof(u_char)); // Make sure there will be '\0' in the end
+        	ngx_memcpy(foundStr, str.data, str.len * sizeof(u_char));
+
+        	// Now we can use sprintf() safely (else it would copy the whole user agent string..)
+        	str.data = p = ngx_alloc(100 * sizeof(u_char), r->connection->log);
+        	ngx_memzero(p, 100 * sizeof(u_char));
+        	p = ngx_sprintf(p, (const char *)cur->replacement->data, foundStr);
+        	*p = '\0';
+        }
+
+        v->data = str.data;
+		v->len = ngx_strlen(str.data);
+		v->valid = 1;
+		v->no_cacheable = 0;
+		v->not_found = 0;
+		break;
+    }
+
+not_found:
+	if (v->valid != 1) {
+		v->not_found = 1;
+	}
     return NGX_OK;
+}
+
+static ngx_array_t * ngx_http_ua_parse_load_from_json(ngx_conf_t *cf, cJSON *current)
+{
+	int            				i, arraySize;
+	ngx_array_t					*lst;
+	cJSON						*arr;
+	ngx_http_ua_parse_elem_t	*elem;
+	ngx_str_t					*str;
+	u_char						errstr[NGX_MAX_CONF_ERRSTR];
+
+	arraySize = cJSON_GetArraySize(current);
+	lst = ngx_array_create(cf->pool, arraySize, sizeof(ngx_http_ua_parse_elem_t));
+	if (lst == NULL) {
+		goto failed;
+	}
+	for (i = 0; i < arraySize; i++) {
+		arr = cJSON_GetArrayItem(current, i);
+		elem = ngx_array_push(lst);
+		if (elem == NULL) {
+			goto failed;
+		}
+		str = ngx_calloc(sizeof(ngx_str_t), cf->log);
+
+		str->data = (u_char *)cJSON_GetObjectItem(arr, "regex")->valuestring;
+		str->len = ngx_strlen(str->data);
+
+		elem->rgc.pattern = *str;
+		elem->rgc.pool = cf->pool;
+		elem->rgc.err.len = NGX_MAX_CONF_ERRSTR;
+		elem->rgc.err.data = errstr;
+		if (ngx_regex_compile(&elem->rgc) != NGX_OK) {
+			goto failed;
+		}
+		if (cJSON_GetObjectItem(arr, "replacement") != NULL) {
+			elem->replacement = ngx_calloc(sizeof(ngx_str_t), cf->log);
+			elem->replacement->data = (u_char *)cJSON_GetObjectItem(arr, "replacement")->valuestring;
+			elem->replacement->len = ngx_strlen(elem->replacement->data);
+		}
+//		ngx_log_error(NGX_LOG_ERR, cf->log, 0, "Iterated idx %d at %d with %s (captures=%d)", i, elem->rgc, elem->rgc.pattern.data, elem->rgc.captures);
+	}
+failed:
+	return lst;
 }
 
 static char *
 ngx_http_ua_parse_list(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
-//     ngx_log_error(NGX_LOG_ERR, cf->log, 0, "Sup?", );
-    ngx_fd_t    fd;
-    ngx_str_t   *value;
-    u_char      *regexFile;
+    ngx_fd_t        fd = NGX_INVALID_FILE;
+    ngx_str_t       *value;
+    u_char          *regexFile;
+    char            *buf = NULL;
+    size_t          len;
+    ngx_file_info_t fi;
+    off_t           size;
+    char*           rc;
+    cJSON           *root;
+    ngx_http_ua_parse_conf_t    *upcf = conf;
+    
+    rc = NGX_CONF_ERROR;
+    
+    if (upcf->devices) {
+        return "duplicate!";
+    }
+    
     value = cf->args->elts;
     regexFile = (u_char*)value[1].data;
     
@@ -98,7 +264,36 @@ ngx_http_ua_parse_list(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
         ngx_log_error(NGX_LOG_CRIT, cf->log, ngx_errno,
                       ngx_open_file_n " \"%s\" failed", regexFile);
     }
-    return NGX_CONF_OK;
+    
+    if (ngx_fd_info(fd, &fi) == NGX_FILE_ERROR) {
+        ngx_log_error(NGX_LOG_ALERT, cf->log, ngx_errno,
+                      ngx_fd_info_n " \"%s\" failed", regexFile);
+        goto failed;
+    }
+    size = ngx_file_size(&fi);
+    
+    len = (off_t) 65536 > size ? (size_t) size : 65536;
+    
+    buf = ngx_alloc(len, cf->log);
+    if (buf == NULL) {
+        goto failed;
+    }
+    
+    // TODO: validate we read it all
+    ngx_read_fd(fd, buf, len);
+    
+    
+    root = cJSON_Parse(buf);
+    
+    // OS
+    // ngx_http_ua_parse_elem_t
+    upcf->os = ngx_http_ua_parse_load_from_json(cf, cJSON_GetObjectItem(root, "os"));
+//    upcf->devices = ngx_http_ua_parse_load_from_json(cf, cJSON_GetObjectItem(root, "devices"));
+//    upcf->browsers = ngx_http_ua_parse_load_from_json(cf, cJSON_GetObjectItem(root, "browsers"));
+
+    ngx_log_error(NGX_LOG_ERR, cf->log, 0, "All ok!");
+    rc = NGX_CONF_OK;
+    
 failed:
     if (fd != NGX_INVALID_FILE) {
         if (ngx_close_file(fd) == NGX_FILE_ERROR) {
@@ -106,7 +301,10 @@ failed:
                           ngx_close_file_n " \"%s\" failed", regexFile);
         }
     }
-    return NGX_CONF_ERROR;
+    if (buf != NULL) {
+        ngx_free(buf);
+    }
+    return rc;
 }
 
 static ngx_int_t
@@ -124,4 +322,22 @@ ngx_http_ua_parse_add_variables(ngx_conf_t *cf)
     }
     
     return NGX_OK;
+}
+
+static void
+ngx_http_ua_parse_cleanup(void *data)
+{
+    ngx_http_ua_parse_conf_t  *upcf = data;
+
+    if (upcf->devices) {
+        ngx_array_destroy(upcf->devices);
+    }
+
+    if (upcf->browsers) {
+        ngx_array_destroy(upcf->browsers);
+    }
+
+    if (upcf->os) {
+        ngx_array_destroy(upcf->os);
+    }
 }
